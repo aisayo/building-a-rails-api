0a95e84b62dc1468b36ed3d5bf59d0a928ac2ac7
Aysan Isayo <aisayo@icloud.com>
2020-07-07 08:28:24 -0700

    added seeded data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+denim = Category.create({ name: "denim" })
+basics = Category.create({ name: "basics" })
+pants = Category.create({ name: "pants" })
+tops = Category.create({ name: "tops" })
+sportswear = Category.create({ name: "sportswear" })
+
+
+
+
+Item.create([
+    { name: "Boyfriend Jeans", price: 30, category: denim}, 
+    { name: "Tee", price: 10, category: basics}, 
+    { name: "Slacks", price: 25, category: pants},
+    { name: "Blouse", price: 50, category: tops},
+    { name: "Leggings", price: 15, category: sportswear}
+])
